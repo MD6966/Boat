801a74ceeeab2f005f9982ee95d431415abcd8c1
Syed Mudasser Anayat <107185992+MD6966@users.noreply.github.com>
2023-09-20 15:06:23 +0500

    .

--- src/views/User/AssignedBoats/AssignedBoats.js
+++ src/views/User/AssignedBoats/AssignedBoats.js
@@ -1,10 +1,18 @@
+import { Box, styled } from '@mui/material'
 import React from 'react'
-
+import Page from '../../../components/page'
+const StyledRoot = styled(Box)(({theme})=> ({
+    padding:theme.spacing(5)
+}))
 const AssignedBoats = () => {
   return (
-    <div>
+    <Page
+    title="Assigned Boats"
+    >
+        <StyledRoot>
       Assigned Baots
-    </div>
+        </StyledRoot>
+    </Page>
   )
 }
 
